bd9d8383ce3afea7329aa98ef1a4c2adcbdb9615
shjWilson <Seunghye.Wilson@agilebeat.com>
2020-05-24 00:07:19 -0400

    add comments

--- convert_to_tfjs.py
+++ convert_to_tfjs.py
@@ -1,5 +1,8 @@
 from tensorflow import keras
+
+#load keras/tensorflow model (e.g. 'model.h5')
 model = keras.models.load_model('./keras-models/motorway_4ch.h5')
 
+#create model in json format to specified folder
 import tensorflowjs as tfjs
-tfjs.converters.save_keras_model(model, tfjs_target_dir)
+tfjs.converters.save_keras_model(model, './tfjs-models/')
